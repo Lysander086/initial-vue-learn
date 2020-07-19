be6175df36fec3ce6d009b1e76442ed609769a1f
Lysander <Lysander@example.com>
2020-07-19 19:43:12 +0800

    插槽的使用:   详见child.vue, Parent.vue

--- src/router.js
+++ src/router.js
@@ -1,25 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Demo from './views/Demo.vue';
-import ModelDemo from './views/ModelDemo.vue';
-import DemoComponent from './views/DemoComponent.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    alias: "/demo",
+    path: "/demo",
     name: 'Demo',
     component: Demo
-  }, {
-    path: "/modelDemo",
-    name: 'modelDemo',
-    component: ModelDemo
-  }, {
-    path: "/demoComponent",
-    name: 'DemoComponent',
-    component: DemoComponent
+  },
+  {
+    path: "/parent",
+    alias: "/",
+    name: 'Parent',
+    component: () => import('@/views/Parent')
   }
 ];
 
